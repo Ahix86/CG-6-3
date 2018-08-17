4267ece6d9e2c41d0358bbca46d9438f3afcb305
Allison <allison.hix86@gmail.com>
2018-08-16 20:21:28 -0500

    Alice

--- CG 6-3/Program.cs
+++ CG 6-3/Program.cs
@@ -4,9 +4,36 @@ namespace CG_6_3
 {
     class Program
     {
+        public static string ALICE { get; private set; }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            string alice = "Alice was beginning to get very tired of sitting by her sister on the bank, and of having nothing to do: once or twice she had peeped into the book her sister was reading, but it had no pictures or conversations in it, 'and what is the use of a book,' thought Alice 'without pictures or conversation?'";
+            // ask the user to search a word
+            Console.Write("Type a word to search:  ");
+            // turn input into string
+            string word = Console.ReadLine();
+
+            // set initial string to upper and lower case and 
+            alice = word.ToLower() + word.ToUpper() + "Alice";
+
+
+            if (alice.Contains(alice))
+            {
+                Console.WriteLine("true");
+            }
+
+            else if (alice.Contains(word))
+            {
+                Console.WriteLine("found");
+            }
+
+
+                Console.ReadLine();
+            }
         }
     }
-}
+
+     
+
+    
